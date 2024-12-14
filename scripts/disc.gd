300a5a962523c54a91e0e70d71761eec626f5895
nonyx777 <nahomretebo@gmail.com>
2024-12-14 16:30:21 +0300

    slerps between orientations, w.r.t ball's velocity rather than raw position

--- scripts/disc.gd
+++ scripts/disc.gd
@@ -1,11 +1,16 @@
 extends CharacterBody3D
 @onready var ball: RigidBody3D = $"../Ball"
 @onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
-@export var theta: float = PI/3
-var new_transform = Transform3D()
+@export var max_angle: float = PI/6
+@export var snappiness: float = 1
 
-func _physics_process(_delta: float):
-	var disc_ball: Vector3 = ball.position - position
+var new_transform = Transform3D()
+var position_tobe = Vector3()
+
+func _physics_process(delta: float):
+	position_tobe = ball.position + ball.linear_velocity
+	
+	var disc_ball: Vector3 = position_tobe - position
 	var length: float = disc_ball.length()
 	var disc_ball_norm: Vector3 = disc_ball.normalized()
 	var rotation_axis: Vector3 = transform.basis.y.normalized().cross(disc_ball_norm)
@@ -13,7 +18,7 @@ func _physics_process(_delta: float):
 	if rotation_axis == Vector3.ZERO:
 		return
 	
-	var desired_rotation = Basis(rotation_axis.normalized(), -theta * length / collision_shape_3d.shape.radius)
+	var desired_rotation = Basis(rotation_axis.normalized(), -max_angle * length / collision_shape_3d.shape.radius)
 	new_transform = transform
-	new_transform.basis = desired_rotation
+	new_transform.basis = new_transform.basis.slerp(desired_rotation, snappiness * delta)
 	transform = new_transform
